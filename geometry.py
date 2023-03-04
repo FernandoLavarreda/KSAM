3cbfe129c77be9550102bf51c12895697f869097
Fernando José Lavarreda Urizar <fernandolavarredau@gmail.com>
2023-03-04 17:48:19 -0600

    4 bar solved and graphics presented

--- geometry.py
+++ geometry.py
@@ -3,7 +3,7 @@
 
 
 from typing import List, Tuple
-from math import sin, cos, pi, sqrt, atan
+from math import sin, cos, pi, sqrt, atan, atan2
 
 
 
@@ -22,7 +22,6 @@ class Vector:
         self.y = y
 
     
-    
     def rotate(self, angle:float):
         self.x, self.y = self.x*cos(angle)-self.y*sin(angle), self.x*sin(angle)+self.y*cos(angle)
     
@@ -36,18 +35,36 @@ class Vector:
         self.x+=x
     
     
-    
     def __add__(self, other):
         return Vector(self.x+other.x, self.y+other.y)
     
     
+    def __sub__(self, other):
+        return Vector(self.x-other.x, self.y-other.y)
+    
+    
     def __str__(self):
         return f"Vector: \nx: {self.x}\ny: {self.y}"
+    
+    
+    def size(self):
+        return sqrt(self.x*self.x+self.y*self.y)
+    
+    
+    def copy(self):
+        return Vector(self.x, self.y)
+
 
 
 def vector_length(a:Vector, b:Vector):
     """Return the distance between two vectors"""
     return sqrt((b.x-a.x)**2+(b.y-a.y)**2)
+
+
+def vector_angle(a:Vector, b:Vector):
+    """Get the angle between two position"""
+    return atan2(b.y-a.y, b.x-a.x)
+
 
 
 class Curve:
@@ -75,6 +92,10 @@ class Curve:
     def absolute(self):
         return Curve(self.origin, [v+self.origin for v in self.vectors])
     
+    
+    def copy(self):
+        return Curve(self.origin.copy(), [v.copy() for v in self.vectors])
+
 
 
 class Link:
@@ -89,17 +110,23 @@ class Link:
     
     
     def rotate(self, angle:float):
-        self.rotate(angle_rad(angle))
+        for curve in self.curves:
+            curve.rotate(angle)
+        
+        for conn in self.connections:
+            conn.rotate(angle)
     
     
     def rotate_angle(self, angle:float):
-        for curve in self.curves:
-            curve.rotate_angle(angle)
+        self.rotate(angle_rad(angle))
     
     
     def translate(self, x:float, y:float):
         for curve in self.curves:
             curve.translate(x, y)
+        
+        for conn in self.connections:
+            conn.translate(x, y)
     
     
     def absolute(self):
@@ -111,17 +138,23 @@ class Link:
         assert a < len(self.connections), "a argument is out of bounds"
         assert b < len(self.connections), "b argument is out of bounds"
         return vector_length(self.connections[a], self.connections[b])
+    
+    
+    def copy(self):
+        return Link(self.origin.copy(), [conn.copy() for conn in self.connections], [c.copy() for c in self.curves], self.thickness)
 
 
 
 class Mechanism:
-    def __init__(self, origin:Vector, links:List[Link], connections:List[Tuple[int, int]]):
+    def __init__(self, origin:Vector, rotation:float, links:List[Link], connections:List[Tuple[int, int]]):
         """Representation of 4 bar mechanism, must be ordered as follows: a (crank), b (coupler), c (output), d (bench/ground)
            connections: numbers indicating the connection points from the links to use"""
         assert len(connections) == len(links), "Missmatch between connections and links"
         assert len(links) == 4, "Can only solve for 4 bar mechanism"
+        
         self.origin = origin
-        self.links = links
+        #self.rotation = rotation
+        self.links = [l.copy() for l in links]
         self.connections = connections
         
         self.a = links[0].length(connections[0][0], connections[0][1])
@@ -133,6 +166,27 @@ class Mechanism:
         self.k3 = (self.a*self.a-self.b*self.b+self.c*self.c+self.d*self.d)/(2*self.a*self.c)
         self.k4 = self.d/self.b
         self.k5 = (self.c*self.c-self.d*self.d-self.a*self.a-self.b*self.b)/(2*self.a*self.b)
+        
+        if rotation > 2*pi:
+            self.rotation = angle_rad(rotation)
+        else:
+            self.rotation = rotation
+        
+        
+        self.links[3].rotate(self.rotation)
+        displacement = origin-links[3].connections[connections[3][0]]
+        self.links[3].translate(displacement.x, displacement.y)
+        
+        for i in range(3):
+            
+            if i == 2:
+                self.links[i].translate(-self.links[i].connections[connections[i][1]].x, -self.links[i].connections[connections[i][1]].y)
+                self.links[i].rotate(vector_angle(self.links[i].connections[connections[i][1]], self.links[i].connections[connections[i][0]])+self.rotation+pi)
+            else:
+                self.links[i].translate(-self.links[i].connections[connections[i][0]].x, -self.links[i].connections[connections[i][0]].y)
+                self.links[i].rotate(-vector_angle(self.links[i].connections[connections[i][0]], self.links[i].connections[connections[i][1]])+self.rotation)
+        
+        
     
     
     def output_rad(self, input_rad):
@@ -143,8 +197,8 @@ class Mechanism:
         C = self.k1-(self.k2+1)*cos(input_rad)+self.k3
         
         try:
-            solution_a = 2*atan(-B+sqrt(B*B-4*A*C)/2/A)
-            solution_b = 2*atan(-B-sqrt(B*B-4*A*C)/2/A)
+            solution_a = 2*atan((-B+sqrt(B*B-4*A*C))/2/A)
+            solution_b = 2*atan((-B-sqrt(B*B-4*A*C))/2/A)
         except ValueError:
             raise ValueError("Crank can't be put in that position")
         
@@ -166,8 +220,8 @@ class Mechanism:
         F = self.k1+(self.k4-1)*cos(input_rad)+self.k5
         
         try:
-            solution_a = 2*atan(-E+sqrt(E*E-4*D*F)/2/D)
-            solution_b = 2*atan(-E-sqrt(E*E-4*D*F)/2/D)
+            solution_a = 2*atan((-E+sqrt(E*E-4*D*F))/2/D)
+            solution_b = 2*atan((-E-sqrt(E*E-4*D*F))/2/D)
         except ValueError:
             raise ValueError("Crank can't be put in that position")
         
@@ -179,6 +233,36 @@ class Mechanism:
         raise a ValueError if the crank can't be placed in that position"""
         a, b = coupler_rad(angle_rad(input_angle))
         return rad_angle(a), rad_angle(b)
+    
+    
+    def solution(self, angle_rad):
+        """Absolute position given an input angle for crank in radians
+           Return value might be: one solution repeated twice, two different solutions
+           or ValueError if this position is not possible"""
+        ca, cb = self.coupler_rad(angle_rad)
+        oa, ob = self.output_rad(angle_rad)
+        
+        crank = self.links[0].copy()
+        coupler = self.links[1].copy()
+        coupler2 = self.links[1].copy()
+        output = self.links[2].copy()
+        output2 = self.links[2].copy()
+        
+        
+        crank.rotate(angle_rad)
+        coupler.rotate(ca)
+        coupler2.rotate(cb)
+        output.rotate(oa)
+        output2.rotate(ob)
+        
+        crank.translate(self.origin.x, self.origin.y)
+        coupler.translate(crank.connections[self.connections[0][1]].x, crank.connections[self.connections[0][1]].y)
+        coupler2.translate(crank.connections[self.connections[0][1]].x, crank.connections[self.connections[0][1]].y)
+        output.translate(self.links[3].connections[self.connections[3][1]].x, self.links[3].connections[self.connections[3][1]].y)
+        output2.translate(self.links[3].connections[self.connections[3][1]].x, self.links[3].connections[self.connections[3][1]].y)
+        
+        
+        return [crank, coupler, output, self.links[3]], [crank, coupler2, output2, self.links[3]]
 
 
 class Machine:
--- graphics.py
+++ graphics.py
@@ -1,8 +1,105 @@
 #Fernando Jose Lavarreda Urizar
 #Program to analyze Mechanisms, Graduation Project UVG
 
+import geometry as gm
+from time import sleep
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 
+def plot_link(link:gm.Link):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    for curve in link.curves:
+        ax.plot([vector.x for vector in curve.vectors], [vector.y for vector in curve.vectors])
+    miny, maxy = ax.get_ylim()
+    minx, maxx = ax.get_xlim()
+    
+    minimun = min([miny, minx])
+    maximum = max([maxy, maxx])
+    
+    ax.set_xlim([minimun, maximum])
+    ax.set_ylim([minimun, maximum])
+    
+    plt.show()
+
+
+
+def plot_mechanism(crank, coupler, output, ground):
+    links = [crank, coupler, output, ground]
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    
+    
+    for link in links:
+        for curve in link.curves:
+            ax.plot([vector.x for vector in curve.vectors], [vector.y for vector in curve.vectors])
+    miny, maxy = ax.get_ylim()
+    minx, maxx = ax.get_xlim()
+    plt.show()
+
+
+def plot_rotation(mechanism:gm.Mechanism, frames:int, inversion:int=0):
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    s1 = mechanism.solution(0)[inversion]
+    
+    lines = []
+    for link in s1:
+        lines.append([])
+        for curve in link.curves:
+            lines[-1].append(*ax.plot([vector.x for vector in curve.vectors], [vector.y for vector in curve.vectors]))
+    
+    def animate(i):
+        radian = 2*gm.pi*i/frames
+        solution = mechanism.solution(radian)[inversion]
+        for link in range(4):
+            counter = 0
+            for curve in solution[link].curves:
+                lines[link][counter].set_data([v.x for v in curve.vectors], [v.y for v in curve.vectors])
+                counter+=1
+        ret = []
+        for ll in lines:
+            for cc in ll:
+                ret.append(cc)
+        return ret
+    
+    anim = FuncAnimation(fig, animate, frames=frames, interval=20, blit=True)
+    plt.show()
+
+
+
+
+
+if __name__ == "__main__":
+    #radii 
+    c1 = gm.Curve(gm.Vector(0, 0), [gm.Vector(x/20, (x/20)**2) for x in range(11)])
+    c2 = gm.Curve(gm.Vector(0, 0), [gm.Vector(x/20, (x/20-1)**2) for x in range(10, 21)])
+    c3 = gm.Curve(gm.Vector(0, 0), [gm.Vector(0, 0), gm.Vector(1, 0)])
+    
+    hc1 = gm.Curve(gm.Vector(0, 0), [gm.Vector(x/40, 1+(0.25-(x/40-0.5)**2)**0.5) for x in range(41)])
+    hc2 = gm.Curve(gm.Vector(0, 0), [gm.Vector(x/40, -1-(0.25-(x/40-0.5)**2)**0.5) for x in range(41)])
+    jcurve = gm.Curve(gm.Vector(0, 0), [gm.Vector(0, 1), gm.Vector(0, -1)])
+    jcurve2 = gm.Curve(gm.Vector(0, 0), [gm.Vector(1, 1), gm.Vector(1, -1)])
+    
+    
+    bc1 = gm.Curve(gm.Vector(0, 0), [gm.Vector(x/20, -(x/20)**2) for x in range(21)])
+    bc2 = gm.Curve(gm.Vector(0, 0), [gm.Vector(x/20, -(x/20-2)**2) for x in range(20, 41)])
+    bc3 = gm.Curve(gm.Vector(0, 0), [gm.Vector(0, 0), gm.Vector(2, 0)])
+    
+    
+    link = gm.Link(gm.Vector(0, 0), [gm.Vector(0, 0), gm.Vector(1, 0)], [c1, c2, c3], 0.0)
+    link2 = gm.Link(gm.Vector(0, 0), [gm.Vector(0.5, 1.25), gm.Vector(0.5, -1.25)], [hc1.copy(), hc2.copy(), jcurve.copy(), jcurve2.copy()], 0.0)
+    link3 = gm.Link(gm.Vector(0, 0), [gm.Vector(0.5, 1.25), gm.Vector(0.5, -1.25)], [hc1, hc2, jcurve, jcurve2], 0.0)
+    link4 = gm.Link(gm.Vector(0, 0), [gm.Vector(0, -0.2), gm.Vector(2, -0.2)], [bc1, bc2, bc3], 0.0)
+    
+    
+    mech = gm.Mechanism(gm.Vector(0, 0), -0.2*gm.pi, [link, link2, link3, link4], ((0, 1), (0, 1), (0, 1), (0, 1)))
+    #plot_link(mech.links[1])
+    #plot_link(mech.solution(0*gm.pi)[1][1])
+    #plot_link(mech.solution(0*gm.pi)[1][0])
+    #plot_mechanism(*mech.solution(0.2*gm.pi)[1])
+    plot_rotation(mech, frames=100, inversion=1)
 
 
 
